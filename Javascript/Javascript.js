1c895ad5e14d10da170beb5ba14871d16098b69c
mmarcomm <mmarcommartin@gmail.com>
2023-09-12 17:13:17 +0100

    Cleaning up code and any errors
    SGV - SVG and meta no links or stuff written

--- Javascript/Javascript.js
+++ Javascript/Javascript.js
@@ -1,21 +1,9 @@
-// Get references to the menu button and the elements to hide
-const menuButton = document.querySelector('.menubutton');
-const square = document.querySelector('.square');
-const navgatorList = document.querySelector('.navbar ul');
+document.addEventListener('DOMContentLoaded', function() {
+  var menuButton = document.querySelector('.menubutton');
 
-// Add a click event listener to the menu button
-menuButton.addEventListener('click', function () {
-  // Toggle the visibility of the square element
-  if (square.style.display === 'none' || square.style.display === '') {
-    square.style.display = 'block';
-  } else {
-    square.style.display = 'none';
-  }
-
-  // Toggle the visibility of the navigation list
-  if (navgatorList.style.display === 'none' || navgatorList.style.display === '') {
-    navgatorList.style.display = 'block';
-  } else {
-    navgatorList.style.display = 'none';
+  if (menuButton) {
+      menuButton.addEventListener('click', function() {
+          window.location.href = 'produto.html';
+      });
   }
 });
